38ecf38202b130eecd5feab6b87fb80369d44e95
HarshalJTemkar <harshal.dragon@gmail.com>
2024-05-07 00:04:26 +0530

    Changes

--- src/main/java/harshal/temkar/notification_service/kafka/model/Reservation.java
+++ src/main/java/harshal/temkar/notification_service/kafka/model/Reservation.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class ReservationKafka  {
+public class Reservation  {
 
 	private String id;
 
--- src/main/java/harshal/temkar/notification_service/kafka/service/ReservationEventConsumer.java
+++ src/main/java/harshal/temkar/notification_service/kafka/service/ReservationEventConsumer.java
@@ -3,10 +3,9 @@ package harshal.temkar.notification_service.kafka.service;
 import java.util.Date;
 
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.listener.ConsumerSeekAware;
 import org.springframework.stereotype.Service;
 
-import harshal.temkar.notification_service.kafka.model.ReservationKafka;
+import harshal.temkar.notification_service.kafka.model.Reservation;
 import harshal.temkar.notification_service.model.Notification;
 import harshal.temkar.notification_service.service.NotificationService;
 import lombok.RequiredArgsConstructor;
@@ -15,16 +14,15 @@ import lombok.extern.log4j.Log4j2;
 @Service
 @RequiredArgsConstructor
 @Log4j2
-public class ReservationEventConsumer implements ConsumerSeekAware {
+public class ReservationEventConsumer {
 
 	private final NotificationService notificationService;
 
 	@KafkaListener(topics = "${kafka.topic.reservationCreated}")
-	public void consumeReservationCreatedEvent(ReservationKafka reservation) {
-		log.info("ReservationEventConsumer ==> consumeReservationCreatedEvent ==> Start");
+	public void consumeReservationRegisteredEvent(Reservation reservation) {
+		log.info("ReservationEventConsumer ==> consumeReservationRegisteredEvent ==> Start");
 		Notification notification = Notification.builder().customerid(reservation.getCustomerid()).date(new Date())
-				.message("Reserved").build();
+				.message("Welcome").build();
 		notificationService.saveNotification(notification);
 	}
-	
 }
